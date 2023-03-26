ffcacf3b7b3ed05edbc49335775a5244bc8389c8
GehadMohamedSalah <38442331+GehadMohamedSalah@users.noreply.github.com>
2023-03-26 21:21:34 +0200

    Delete YallaBaity/Areas/Api/ViewModel directory

--- YallaBaity/Areas/Api/ViewModel/VmFoodOrderWithDistance.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace YallaBaity.Areas.Api.ViewModel
-{
-    public class VmFoodOrderWithDistance
-    {
-        public double Distance { get; set; }
-        public VmFoodOrder VmFoodOrder { get; set; }
-    }
-}
