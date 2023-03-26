16e337a520bc27869337924e45176b39b6ece27d
GehadMohamedSalah <38442331+GehadMohamedSalah@users.noreply.github.com>
2023-03-26 21:23:32 +0200

    Delete YallaBaity/Areas/Api/Services directory

--- YallaBaity/Areas/Api/Services/ILinqServices.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace YallaBaity.Areas.Api.Services
-{
-    public interface ILinqServices
-    {
-        string GenerateQuery<T>(string includes = "",string additions="");
-        public T GetDefaultIsEmpty<T>(int id) where T : class;
-    }
-}
